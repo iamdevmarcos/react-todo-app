953718e9812334288a17c3dd794a82c290660591
Marcos Andre <marcosdev.me@gmail.com>
2021-11-28 13:30:19 -0300

    creating initial style and loading state

---
+++ src/App.styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color:#17181F;
+    color:#797A81;
+    min-height:100vh;
+`;
+
+export const Area = styled.div`
+    margin:auto;
+    padding:20px;
+    max-width:980px;
+
+    .loading {
+        text-align:center;
+        padding-top:50px;
+        color:#FFF;
+        font-size:20px;
+    }
+`;
+
+export const Header = styled.h1`
+    margin:0;
+    padding:0;
+    color:#FFF;
+    text-align:center;
+    border-bottom: 1px solid #CCC;
+    padding-bottom:20px;
+`;
